6e8e977387dee7d19c5c87ced901dcbfff476165
Saurabh <s.yadav0791@gmail.com>
2017-01-09 23:42:46 +0530

    Initial Commit

---
+++ SwiftOCRDemo_8.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftOCRDemo_8.2
+//
+//  Created by Saurabh Yadav on 09/01/17.
+//  Copyright © 2017 Saurabh Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
