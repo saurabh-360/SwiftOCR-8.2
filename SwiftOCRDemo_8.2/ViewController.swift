7abef278e1f49c8e2e81e1e0d2e161f27e0dd8d5
Saurabh <s.yadav0791@gmail.com>
2017-01-10 00:16:06 +0530

    Support for 8.2

--- SwiftOCRDemo_8.2/ViewController.swift
+++ SwiftOCRDemo_8.2/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import SwiftOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    let picker = UIImagePickerController()
+    private let ocrInstance = SwiftOCR()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
+//
+        self.ocrInstance.recognize(UIImage.init(named: "imageTest")!) { [weak self] recognizedString in
+            DispatchQueue.main.async {
+                print(recognizedString)
+                print(self?.ocrInstance.currentOCRRecognizedBlobs ?? "Recoginzed Blob is empty")
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +32,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func photofromLibrary(_ sender: UIButton) {
+        
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        
+        self.ocrInstance.recognize(chosenImage) { [weak self] recognizedString in
+            DispatchQueue.main.async {
+                print(recognizedString)
+                print(self?.ocrInstance.currentOCRRecognizedBlobs ?? "Recoginzed Blob is empty")
+            }
+        }
+        dismiss(animated:true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
